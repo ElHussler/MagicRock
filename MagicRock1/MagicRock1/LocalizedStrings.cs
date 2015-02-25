bd759f2630a49897cdcecceeca21b62616073e1e
Luke <LukeHussey@users.noreply.github.com>
2015-02-25 19:39:15 +0000

    First commit

---
+++ MagicRock1/MagicRock1/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MagicRock1.Resources;
+
+namespace MagicRock1
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
